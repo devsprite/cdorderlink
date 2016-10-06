d66844300693fad1d8e4b570cddd8903d65f3c38
Dominique <dominique@chez-dominique.fr>
2016-10-06 11:27:12 +0200

    fix licence space

--- cdorderlink.php
+++ cdorderlink.php
@@ -18,8 +18,8 @@
 * versions in the future. If you wish to customize PrestaShop for your
 * needs please refer to http://www.prestashop.com for more information.
 *
-*  @author Dominique <dominique@chez-dominique.fr>
-*  @copyright  2007-2016 Chez-Dominique
+*  @author    Dominique <dominique@chez-dominique.fr>
+*  @copyright   2007-2016 Chez-Dominique
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
